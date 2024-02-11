271c32ba10afff5ff94290672ff9fc31c08bc702
Wesley00s <wesley300rodrigues@gmail.com>
2024-02-11 15:15:04 -0300

    Persistence Layer

---
+++ src/main/java/com/wesley/restaurant/application/system/entity/Table.java
@@ -0,0 +1,47 @@
+package com.wesley.restaurant.application.system.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Table {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long tableId;
+
+    @Column(nullable = false)
+    private int tableCapacity;
+
+    private boolean available;
+
+    public Table(int tableCapacity, boolean available) {
+        this.tableCapacity = tableCapacity;
+        this.available = available;
+    }
+
+    public Table() {
+
+    }
+
+    public long getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(long tableId) {
+        this.tableId = tableId;
+    }
+
+    public int getTableCapacity() {
+        return tableCapacity;
+    }
+
+    public void setTableCapacity(int tableCapacity) {
+        this.tableCapacity = tableCapacity;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
