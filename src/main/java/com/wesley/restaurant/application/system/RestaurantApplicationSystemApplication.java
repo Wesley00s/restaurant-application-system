5f7b0e553a12c6e82888c67628947c9b205bee41
Wesley00s <wesley300rodrigues@gmail.com>
2024-02-11 10:43:10 -0300

    commit init

---
+++ src/main/java/com/wesley/restaurant/application/system/RestaurantApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wesley.restaurant.application.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplicationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplicationSystemApplication.class, args);
+	}
+
+}
