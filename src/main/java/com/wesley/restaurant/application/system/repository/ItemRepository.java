f14b14fe4758c6747a98e11d6a5ff71f8b263f40
Wesley R. S <130418293+Wesley00s@users.noreply.github.com>
2024-02-11 15:36:33 -0300

    Revert "Persistence Layer"

--- src/main/java/com/wesley/restaurant/application/system/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wesley.restaurant.application.system.repository;
-
-import com.wesley.restaurant.application.system.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- src/main/java/com/wesley/restaurant/application/system/repository/TableRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wesley.restaurant.application.system.repository;
-
-import com.wesley.restaurant.application.system.entity.Table;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TableRepository extends JpaRepository<Table, Long> {
-}
